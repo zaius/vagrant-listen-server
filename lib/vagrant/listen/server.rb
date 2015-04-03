b316d5265ec69e5fa58cd96e7bb587b2180d2036
David Kelso <david@kelso.id.au>
2015-04-03 16:04:39 -0700

    Gem scaffold.

---
+++ lib/vagrant/listen/server.rb
@@ -0,0 +1,9 @@
+require "vagrant/listen/server/version"
+
+module Vagrant
+  module Listen
+    module Server
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/vagrant/listen/server/version.rb
@@ -0,0 +1,7 @@
+module Vagrant
+  module Listen
+    module Server
+      VERSION = "0.0.1"
+    end
+  end
+end
